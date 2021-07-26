7c6f9740aae263524e3b4f80422f23ff08398808
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-07-26 15:08:26 -0400

    file contain list of soft and tehnical skills.

---
+++ ryan-react-portfolio/src/skillsList.js
@@ -0,0 +1,6 @@
+const skills = {
+    technical: ["HTML","CSS","JavaScript","Node.js","Express","mySql","Mongodb", "React","GraphQl"],
+    soft: ['communication','team work', 'helpful', 'willing to seek help', 'patience', 'open-minded', 'problem solving', 'accountability', 'creative']
+}
+
+export default skills;
