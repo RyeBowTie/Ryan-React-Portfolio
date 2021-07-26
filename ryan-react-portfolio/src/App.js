7a7ac3e8b3fcc8f3e8ed002f61141f0227c62203
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-07-26 13:36:34 -0400

    added render page function.

--- ryan-react-portfolio/src/App.js
+++ ryan-react-portfolio/src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/header';
-// import Footer from './components/footer';
+import Footer from './components/footer';
 import About from './components/about';
-// import Projects from './components/projects';
-// import Contact from './components/contact';
-// import Resume from './components/resume';
+import Projects from './components/projects';
+import Contact from './components/contact';
+import Resume from './components/resume';
+
+import projects from './projectInfo';
+import resumePDF from './assets/ryan_odonohue_resume_2021.pdf'; 
+
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('about');
+
+  const renderPage = () => {
+    if (currentPage === 'about') {
+      return <About/>;
+    } else if (currentPage === 'projects') {
+      return <Projects projects={projects}/>
+    } else if (currentPage === 'contact') {
+      return <Contact/>
+    } else if (currentPage === 'resume') {
+      return <Resume resume={resumePDF}/>
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
   return (
     <div className="App">
-      <Header />
-      < About />
+      <Header  handlePageChange= {handlePageChange}/>
+      {renderPage()}
+      <Footer />
     </div>
   );
 }
