0d65890b555a5ba4c938e695a439903a81784fb2
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-07-26 13:35:07 -0400

    file containing info on projects.

---
+++ ryan-react-portfolio/src/projectInfo.js
@@ -0,0 +1,40 @@
+import Krashians from './assets/Project1a.png';
+import Blog from './assets/main-loggedin.png';
+import Profile from './assets/3.png';
+import Forcast from './assets/Forecast2.png';
+import Quiz from './assets/Quiz1.png'
+
+const projectArray = [
+    {
+        name: 'Avoiding the Kar Krashians',
+        github: 'https://github.com/RyeBowTie/Car-Crashians',
+        deployed: 'https://ryebowtie.github.io/Car-Crashians/',
+        img: Krashians,
+    },
+    {
+        name: 'Tech Blog',
+        github: 'https://github.com/RyeBowTie/Ryan-Tech-Blog',
+        deployed: 'http://ryantechblog.herokuapp.com/',
+        img: Blog,
+    },
+    {
+        name: 'JavaScript Quiz',
+        github: 'https://github.com/RyeBowTie/Ryan-s-Javascript-Quiz',
+        deployed: 'https://ryebowtie.github.io/Ryan-s-Javascript-Quiz/.',
+        img: Quiz,
+    },
+    {
+        name: 'Team Profile Generator',
+        github: 'https://github.com/RyeBowTie/Ryan-Team-Profile-Generator',
+        deployed: 'https://github.com/RyeBowTie/Ryan-Team-Profile-Generator',
+        img: Profile,
+    },
+    {
+        name: 'Weather Forecaster',
+        github: 'https://ryebowtie.github.io/Ryan-weather-forecaster-/',
+        deployed: 'https://ryebowtie.github.io/Ryan-weather-forecaster-/',
+        img: Forcast,
+    },
+]
+
+export default projectArray;
